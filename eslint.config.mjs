bf871abc901cde00596553d0b87d639a6b249388
Riku Lindblad <riku.lindblad@iki.fi>
2025-06-22 00:42:11 +0300

    Update packages

---
+++ eslint.config.mjs
@@ -0,0 +1,36 @@
+import js from "@eslint/js";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsparser from "@typescript-eslint/parser";
+import globals from "globals";
+
+export default [
+  js.configs.recommended,
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: tsparser,
+      ecmaVersion: 2022,
+      sourceType: "module",
+      globals: {
+        ...globals.node,
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+        },
+      ],
+      "no-unused-vars": "off", // Turn off base rule in favor of TypeScript version
+    },
+  },
+  {
+    ignores: ["dist/", "node_modules/"],
+  },
+];
